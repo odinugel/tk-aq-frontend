e70f8650275149eb4e27a13a60954708c48aef79
odinugel <odin.ugelstad@gmail.com>
2022-02-27 11:46:51 +0100

    barebones fetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(null);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseData) => setData(responseData));
+  }, [url]);
+
+  return { data };
+}
+export default useFetch;
