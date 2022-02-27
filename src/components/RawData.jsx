cb9556274dc597d9bd6295cb3bc77d96640f2178
odinugel <odin.ugelstad@gmail.com>
2022-02-27 13:13:17 +0100

    Added RawData component and example data json

---
+++ src/components/RawData.jsx
@@ -0,0 +1,84 @@
+import {
+  exampleDustData, exampleGasData, exampleWeatherData, exampleSensorList,
+} from '../exampleData';
+
+export default function RawData() {
+  const date = new Date(exampleDustData[0].timestamp);
+  const sortedSensorList = exampleSensorList.sort((a, b) => a.deviceNo - b.deviceNo);
+  return (
+    <>
+      <h2>
+        Sensor ID:
+        {' '}
+        {exampleDustData[0].deviceID}
+        {' '}
+        &quot;fetched&quot;:
+        {' '}
+        {date.toUTCString()}
+      </h2>
+      <h3>Dust values</h3>
+      <p>
+        PM10 value:
+        {' '}
+        {exampleDustData[0].PM10}
+        <br />
+        PM2.5 value:
+        {' '}
+        {exampleDustData[0]['PM2.5']}
+      </p>
+      <h3>Gas values</h3>
+      <p>
+        NO2 value:
+        {' '}
+        {exampleGasData[0].NO2}
+        <br />
+        O3 value:
+        {' '}
+        {exampleGasData[0].O3}
+        <br />
+        NO value:
+        {' '}
+        {exampleGasData[0].NO}
+      </p>
+      <h3>Weather data</h3>
+      <p>
+        Temperature:
+        {' '}
+        {exampleWeatherData.data[0].temperature}
+        <br />
+        Humidity:
+        {' '}
+        {exampleWeatherData.data[0].humidity}
+        <br />
+        Precipitation:
+        {' '}
+        {exampleWeatherData.data[0].precipitation}
+        <br />
+      </p>
+      <h2>Sensor list</h2>
+      <ul>
+        {sortedSensorList.map((sensor) => (
+          <li>
+            #
+            {sensor.deviceNo}
+            {' '}
+            {sensor.deviceID}
+            {' '}
+            {sensor.deviceName}
+            {' '}
+            {sensor.lastReceivedMsg}
+          </li>
+        ))}
+
+      </ul>
+      <h2>Raw JSON:</h2>
+      <h4>Dust data</h4>
+      <p>{JSON.stringify(exampleDustData)}</p>
+      <h4>Gas data</h4>
+      <p>{JSON.stringify(exampleGasData)}</p>
+      <h4>Weather data</h4>
+      <p>{JSON.stringify(exampleWeatherData)}</p>
+
+    </>
+  );
+}
---
+++ src/exampleData.js
@@ -0,0 +1,89 @@
+export const exampleDustData = [
+  {
+    deviceID: '2f3a11687f7a2h',
+    timestamp: 1645956000000,
+    PM10: 0.7,
+    'PM2.5': 0.6,
+  },
+];
+
+export const exampleGasData = [{
+  deviceID: '2f3a11687f7a2j',
+  timestamp: 1645876800000,
+  NO2: 293.5,
+  O3: 163.5,
+  NO: 348.9,
+}];
+
+export const exampleWeatherData = {
+  sensorName: 'Sverresborg',
+  distanceKm: 0,
+  data: [{
+    time: 1645315200000,
+    temperature: -2,
+    humidity: 82,
+    precipitation: 4.3,
+  }, {
+    time: 1645401600000,
+    temperature: -2.6,
+    humidity: 84,
+    precipitation: 1.7,
+  }, {
+    time: 1645488000000,
+    temperature: -2.4,
+    humidity: 82,
+    precipitation: 0.3,
+  }],
+};
+
+export const exampleSensorList = [{
+  deviceID: 'test', deviceName: 'Inne på TIP', deviceNo: -1, visible: 0, lat: 0, lon: 0, lastReceivedMsg: 0, nearestFrostSensorId: null,
+}, {
+  deviceID: '2f3a11687f7a2j', deviceName: 'Trondheim Torg', deviceNo: 24, visible: 1, lat: 1.1070708, lon: 0.18140128, lastReceivedMsg: 1645963184539, nearestFrostSensorId: null,
+}, {
+  deviceID: '2f3a11687f7a2i', deviceName: 'Charlottenlund barnehage', deviceNo: 20, visible: 1, lat: 1.1069226, lon: 0.18310219, lastReceivedMsg: 1645963292537, nearestFrostSensorId: null,
+}, {
+  deviceID: '2f3a11687f7a2h', deviceName: 'Bispehaugen skole', deviceNo: 3, visible: 1, lat: 1.1070876, lon: 0.1817813, lastReceivedMsg: 1645963171387, nearestFrostSensorId: null,
+}, {
+  deviceID: '2f3a11687f7a2g', deviceName: 'Berg skole', deviceNo: 5, visible: 1, lat: 1.106872, lon: 0.18179303, lastReceivedMsg: 0, nearestFrostSensorId: null,
+}, {
+  deviceID: '2f3a11687f7a2f', deviceName: 'Angelltrøa barnehage', deviceNo: 22, visible: 1, lat: 1.1068274, lon: 0.18258907, lastReceivedMsg: 1645963113826, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg89l', deviceName: 'Elgeseter gate 24', deviceNo: 23, visible: 1, lat: 1.1068766, lon: 0.18145257, lastReceivedMsg: 1645963183419, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg89j', deviceName: 'Tanem skole (Tanemshallen)', deviceNo: 7, visible: 1, lat: 1.1048998, lon: 0.18195747, lastReceivedMsg: 1645963263821, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg89h', deviceName: 'Hesteskoen barnehage', deviceNo: 21, visible: 1, lat: 1.1047899, lon: 0.18287924, lastReceivedMsg: 1645963317359, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg89f', deviceName: 'Lilleby skole', deviceNo: 10, visible: 1, lat: 1.1072512, lon: 0.18212579, lastReceivedMsg: 1645963228916, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg89b', deviceName: 'Sandbakken barnehage', deviceNo: 19, visible: 1, lat: 1.1055098, lon: 0.18088411, lastReceivedMsg: 1645963282780, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg899', deviceName: 'Sjetne skole', deviceNo: 18, visible: 1, lat: 1.1059904, lon: 0.1814329, lastReceivedMsg: 1645963214012, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg897', deviceName: 'Åsvang skole', deviceNo: 17, visible: 1, lat: 1.1067463, lon: 0.18243584, lastReceivedMsg: 1645963305301, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg88k', deviceName: 'Huseby barneskole', deviceNo: 16, visible: 1, lat: 1.10599, lon: 0.18067709, lastReceivedMsg: 1645963300675, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg88i', deviceName: 'Tiller HVS', deviceNo: 15, visible: 1, lat: 1.1057844, lon: 0.18114989, lastReceivedMsg: 1645963138218, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg88g', deviceName: 'Byneset og Nypantunet HVS', deviceNo: 14, visible: 1, lat: 1.1057613, lon: 0.17737113, lastReceivedMsg: 1645963204784, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg88e', deviceName: 'Ila skole', deviceNo: 13, visible: 1, lat: 1.1070707, lon: 0.18100958, lastReceivedMsg: 1645963194901, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg88b', deviceName: 'Utleira skole', deviceNo: 12, visible: 1, lat: 1.1063458, lon: 0.18200785, lastReceivedMsg: 1645963320219, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg889', deviceName: 'Strindheim skole', deviceNo: 11, visible: 1, lat: 1.1071166, lon: 0.18247938, lastReceivedMsg: 1645963310259, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg885', deviceName: 'Sjøskogbekken Fus Barnehage', deviceNo: 9, visible: 1, lat: 1.107008, lon: 0.18366742, lastReceivedMsg: 1645963209275, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg881', deviceName: 'Åsheim barneskole', deviceNo: 8, visible: 1, lat: 1.1056737, lon: 0.18051931, lastReceivedMsg: 1645963203343, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg785', deviceName: 'Lademoen stasjon', deviceNo: 6, visible: 1, lat: 1.1071867, lon: 0.18181898, lastReceivedMsg: 0, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg781', deviceName: 'Singsaker skole', deviceNo: 4, visible: 1, lat: 1.1069458, lon: 0.18175815, lastReceivedMsg: 1645963184939, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg77l', deviceName: 'Ugla skole', deviceNo: 2, visible: 1, lat: 1.1065307, lon: 0.18042946, lastReceivedMsg: 1645963195556, nearestFrostSensorId: null,
+}, {
+  deviceID: '17dh0cf43jg77j', deviceName: 'Sverresborg skole', deviceNo: 1, visible: 1, lat: 1.1068553, lon: 0.1809291, lastReceivedMsg: 1645963113947, nearestFrostSensorId: null,
+}];
