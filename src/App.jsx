3c41bb88b0ef29c4d30e8827937bf13f57c01a22
odinugel <odin.ugelstad@gmail.com>
2022-02-27 12:03:45 +0100

    Showing placholder data due to CORS on AQ-server

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import './App.css';
 import { Stack, Divider } from '@mui/material';
 import Donut from './components/Donut';
 import AccordionAQ from './components/Accordion';
+import useFetch from './hooks/useFetch';
 
 function App() {
+  const { data } = useFetch('https://jsonplaceholder.typicode.com/todos/1');
   return (
     <Stack divider={<Divider />}>
       <Donut size={300} color="success" value={11} text="God" />
       <Donut size={400} color="warning" value={40} text="Moderat" />
       <Donut size={550} color="error" value={85} text="Dårlig" />
       <AccordionAQ />
+      <p>{JSON.stringify(data)}</p>
     </Stack>
   );
 }
