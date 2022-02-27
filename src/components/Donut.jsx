8702994c0bb9ddb5f7ea5afd1ad542275fac8544
odinugel <odin.ugelstad@gmail.com>
2022-02-27 15:11:15 +0100

    Added margin to Donut

--- src/components/Donut.jsx
+++ src/components/Donut.jsx
@@ -17,6 +17,7 @@ export default function Donut({
     <Stack
       justifyContent="center"
       alignItems="center"
+      m="1rem"
     >
       <Box sx={{
         transform: 'rotate(180deg)', zIndex: '99',
