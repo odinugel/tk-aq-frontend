da2c63d57b6b16ed115824796da356c272cb67c4
Fabio <Fabio.rolfsen@outlook.com>
2022-03-02 17:11:23 +0100

    Prøvd å lage SDC

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -6,12 +6,17 @@ import {
 } from '@mui/material';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Donut from './Donut';
+import RawData from './RawData';
+
 
 function AccordionAQ() {
   return (
     <div>
       <Accordion>
         <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography>
+            Panel 1
+          </Typography>
           <Donut size={50} value={25} color="success" />
         </AccordionSummary>
         <AccordionDetails>
@@ -22,6 +27,7 @@ function AccordionAQ() {
       </Accordion>
       <Accordion>
         <AccordionSummary expandIcon={<ExpandMore />}>
+          <Donut size={50} color="success" value={<RawData value={gas[0].NO2}/>}/>
           <Typography>
             Panel 2
           </Typography>
