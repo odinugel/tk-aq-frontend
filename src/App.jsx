7f7e6d2ef89f6c0c696ce275ecd89566ebce5abf
odinugel <odin.ugelstad@gmail.com>
2022-03-10 18:33:21 +0100

    Extracted primary donut and weather into component.
    Co-authored-by: FabioEDGD <FabioEDGD@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Stack, Paper, Typography } from '@mui/material';
+import { Stack, Paper } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
-import { formatDistanceStrict } from 'date-fns';
 import theme from './theme';
-import Donut from './components/Donut';
 import AccordionAQ from './components/Accordion';
 import RawData from './components/RawData';
 import fetchData from './api/fetchData';
-import Weather from './components/Weather';
-import findSensorName from './utils/findSensorName';
 import Loader from './components/Loader';
+import PrimaryDisplay from './components/PrimaryDisplay';
 
 function App() {
   const [data, setData] = useState({});
@@ -25,26 +22,7 @@ function App() {
         <Loader />
       ) : (
         <Stack spacing="1rem" m="1rem">
-          <Paper>
-            <Stack sx={{ alignItems: 'center' }}>
-              <Typography variant="h4" m="1rem">
-                {findSensorName(data.sensorID, data.sensors)}
-              </Typography>
-              <Typography variant="h6" mb="1rem">
-                Last update:
-                {' '}
-                {formatDistanceStrict(data.timestamp, new Date(), { addSuffix: true })}
-              </Typography>
-            </Stack>
-            <Donut
-              size="60%"
-              text // if omitted, no text will be displayed inside circle
-              value={data.topPollutant.value}
-              category={data.topPollutant.category}
-              thickness={2}
-            />
-            <Weather temperature={data.weather.temperature} humidity={data.weather.humidity} />
-          </Paper>
+          <PrimaryDisplay data={data} />
           <AccordionAQ pollutants={data.pollutants} />
           <Paper sx={{ padding: '1rem' }}>
             <RawData data={data} />
---
+++ src/components/PrimaryDisplay.jsx
@@ -0,0 +1,35 @@
+import { Paper, Typography, Stack } from '@mui/material';
+import formatDistanceStrict from 'date-fns/formatDistanceStrict';
+import PropTypes from 'prop-types';
+import Donut from './Donut';
+import Weather from './Weather';
+import findSensorName from '../utils/findSensorName';
+
+export default function PrimaryDisplay({ data }) {
+  return (
+    <Paper>
+      <Stack sx={{ alignItems: 'center' }}>
+        <Typography variant="h4" m="1rem">
+          {findSensorName(data.sensorID, data.sensors)}
+        </Typography>
+        <Typography variant="h6" mb="1rem">
+          Last update:
+          {' '}
+          {formatDistanceStrict(data.timestamp, new Date(), { addSuffix: true })}
+        </Typography>
+      </Stack>
+      <Donut
+        size="60%"
+        text // if omitted, no text will be displayed inside circle
+        value={data.topPollutant.value}
+        category={data.topPollutant.category}
+        thickness={2}
+      />
+      <Weather temperature={data.weather.temperature} humidity={data.weather.humidity} />
+    </Paper>
+  );
+}
+
+PrimaryDisplay.propTypes = {
+  data: PropTypes.object.isRequired,
+};
