d39e2dda6f6660a0d6f27060de92235b7d77affa
marianneslette <71381839+marianneslette@users.noreply.github.com>
2022-03-21 20:44:04 +0100

    Update Header.jsx
    Added logo,  outlined buttons to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line import/no-duplicates
-import { Typography, Box } from '@mui/material';
+import { Typography } from '@mui/material';
 // eslint-disable-next-line import/no-duplicates
 import { Button } from '@mui/material';
 import CardMedia from '@mui/material/CardMedia';
+import Card from '@mui/material/Card';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import LanguageIcon from '@mui/icons-material/Language';
 
 export default function Header() {
   return (
-    <Box sx={{
+    <Card sx={{
       display: 'grid',
       placeItems: 'center',
       backgroundColor: '#ffffff',
@@ -16,24 +17,21 @@ export default function Header() {
 
     }}
     >
-
       <Typography variant="h5">
-        <Button>
+        <Button variant="outlined" startIcon={<ArrowBackIcon />}>
           {' '}
-          <ArrowBackIcon />
           Tilbake
         </Button>
         <CardMedia
           component="img"
-          image="./banner2.png"
+          image="https://tipqa.trondheim.kommune.no/luftkvalitet-frontend/static/media/Horisontal%20standard.3918fc07.svg"
           alt="logo"
         />
         TRONDHEIM KOMMUNE
-        <Button>
-          <LanguageIcon />
+        <Button variant="outlined" startIcon={<LanguageIcon />}>
           Språk
         </Button>
       </Typography>
-    </Box>
+    </Card>
   );
 }
