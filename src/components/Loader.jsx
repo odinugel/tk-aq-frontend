439f9262989b70d4a508568f2e4e08834b5133ac
alehovm <alehovm@gmail.com>
2022-03-22 21:43:54 +0100

    force load

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,43 +1,25 @@
-import * as React from 'react';
 import Skeleton from '@mui/material/Skeleton';
-import Stack from '@mui/material/Stack';
+import { Paper, Typography, Stack } from '@mui/material';
 
-export default function Variants() {
+export default function Loader() {
   return (
-    <Stack
-      height="100%"
-      spacing={1}
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-
-    >
-
+    <Paper>
+      <Stack sx={{ alignItems: 'center' }}>
+        <Typography variant="h4" m="1rem" width="60%">
+          <Skeleton />
+        </Typography>
+        <Typography variant="h6" mb="1rem">
+          <Skeleton />
+        </Typography>
+      </Stack>
       <Skeleton
+        width="60%"
         height="100%"
-        width="100%"
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      />
-      <Skeleton
-        height="100%"
-        sx={{
-          width: '100%',
-          alignItems: 'baseline',
-        }}
         variant="circular"
-      />
-      <Skeleton
-        width="100%"
-        height="100%"
-      />
 
-    </Stack>
+      />
+      <Skeleton />
+    </Paper>
 
   );
 }
