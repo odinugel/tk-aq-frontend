810918cbe0c963669a224c6601bfaee94370a174
alehovm <alehovm@gmail.com>
2022-03-22 20:48:35 +0100

    add skeleton loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,43 @@
-import { Box, CircularProgress } from '@mui/material';
+import * as React from 'react';
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
 
-export default function Loader() {
+export default function Variants() {
   return (
-    <Box sx={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
-      <CircularProgress size={250} thickness={2} />
-    </Box>
+    <Stack
+      height="100%"
+      spacing={1}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+
+    >
+
+      <Skeleton
+        height="100%"
+        width="100%"
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      />
+      <Skeleton
+        height="100%"
+        sx={{
+          width: '100%',
+          alignItems: 'baseline',
+        }}
+        variant="circular"
+      />
+      <Skeleton
+        width="100%"
+        height="100%"
+      />
+
+    </Stack>
+
   );
 }
