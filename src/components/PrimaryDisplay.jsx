4971990f984981fac3ca707e3db9ece1058b45a4
marianneslette <71381839+marianneslette@users.noreply.github.com>
2022-04-08 10:29:10 +0200

    added shortinfo under donut, needs alignment

--- src/components/PrimaryDisplay.jsx
+++ src/components/PrimaryDisplay.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import Donut from './Donut';
 import Weather from './Weather';
 import PrimaryDisplayLoader from './PrimaryDisplayLoader';
+import ShortInfo from './ShortInfo';
 
 export default function PrimaryDisplay({ data, loading }) {
   if (loading) {
@@ -26,6 +27,10 @@ export default function PrimaryDisplay({ data, loading }) {
         category={data.topPollutant.category}
         thickness={2}
       />
+      <br />
+      <Stack sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <ShortInfo />
+      </Stack>
       <Weather temperature={data.weather.temperature} humidity={data.weather.humidity} />
     </Paper>
   );
