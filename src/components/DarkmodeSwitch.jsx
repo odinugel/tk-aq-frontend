b5f521d453be4e2fe8dab363892716bbc087da28
odinugel <odin.ugelstad@gmail.com>
2022-04-13 10:58:31 +0200

    Slider modification

--- src/components/DarkmodeSwitch.jsx
+++ src/components/DarkmodeSwitch.jsx
@@ -1,22 +1,15 @@
-import { useRef } from 'react';
-import {
-  Switch, Stack, Slide, Box,
-} from '@mui/material';
+import { Switch, Stack } from '@mui/material';
 import Brightness3Icon from '@mui/icons-material/Brightness3';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import PropTypes from 'prop-types';
 
 export default function DarkModeSwitch({ darkMode, setDarkMode }) {
-  const containerRef = useRef(null);
+  console.log(darkMode);
   return (
-    <Box ref={containerRef}>
-      <Stack direction="row" alignItems="center">
-        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-        <Slide direction="up" in={darkMode} container={containerRef.current}>
-          { darkMode ? <Brightness3Icon /> : <LightModeIcon /> }
-        </Slide>
-      </Stack>
-    </Box>
+    <Stack direction="row" alignItems="center">
+      <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+      { darkMode ? <Brightness3Icon sx={{ color: 'contrast.main' }} /> : <LightModeIcon /> }
+    </Stack>
   );
 }
 DarkModeSwitch.propTypes = {
