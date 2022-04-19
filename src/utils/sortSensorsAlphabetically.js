fe4023c79479da2a98b8308b8e1da7e07d3adff5
odinugel <odin.ugelstad@gmail.com>
2022-04-19 18:24:50 +0200

    Removed log

--- src/utils/sortSensorsAlphabetically.js
+++ src/utils/sortSensorsAlphabetically.js
@@ -1,5 +1,4 @@
 export default function sortSensorsAlphabetically(sensors) {
   const alphabet = sensors.sort((a, b) => a.deviceName.localeCompare(b.deviceName, 'NO'));
-  console.log(alphabet);
   return alphabet;
 }
