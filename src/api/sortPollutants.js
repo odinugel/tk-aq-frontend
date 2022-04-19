fa036e62ed0018670bd7638b85de2093aed2adf7
odinugel <odin.ugelstad@gmail.com>
2022-04-19 16:49:40 +0200

    Removed comment

--- src/api/sortPollutants.js
+++ src/api/sortPollutants.js
@@ -1,6 +1,4 @@
 export default function sortPollutants(pollutants) {
-  // TODO: Sort pollutants by category in addition to percentage,
-  // change category from string to number in order to sort by category
   return pollutants
     .sort((pollutant, prevPollutant) => prevPollutant.percentage - pollutant.percentage)
     .sort((pollutant, prevPollutant) => prevPollutant.category - pollutant.category);
