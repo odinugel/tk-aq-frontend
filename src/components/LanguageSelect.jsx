fc3f94cf842f2e395c68b4e140a18d840d7ec454
odinugel <odin.ugelstad@gmail.com>
2022-04-24 22:46:28 +0200

    Reversed order

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -17,17 +17,17 @@ export default function LanguageSelect() {
   return (
     <List>
       <Typography margin="1rem 0">{translations.langBtn[language]}</Typography>
-      <ListItemButton selected={(language === 'en')} onClick={() => setLanguage('en')}>
-        <Stack direction="row" spacing={1}>
-          <img src={britflag} width="30px" alt="British flag" />
-          <span>English</span>
-        </Stack>
-      </ListItemButton>
-      <Divider />
       <ListItemButton selected={(language === 'no')} onClick={() => setLanguage('no')}>
         <Stack direction="row" spacing={1}>
           <img src={noflag} width="30px" alt="Norwegian flag" />
           <span>Norsk</span>
+        </Stack>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton selected={(language === 'en')} onClick={() => setLanguage('en')}>
+        <Stack direction="row" spacing={1}>
+          <img src={britflag} width="30px" alt="British flag" />
+          <span>English</span>
         </Stack>
       </ListItemButton>
       <Divider />
