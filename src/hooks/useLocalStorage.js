5b22fdf17c2855b13b76127dff6455795880ed9f
odinugel <odin.ugelstad@gmail.com>
2022-05-01 16:41:10 +0200

    Removed logs

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 function getSavedValue(key, initialValue) {
   const savedValue = JSON.parse(localStorage.getItem(key));
-  console.log(`${key} saved value: ${savedValue}`);
   if (savedValue) return savedValue;
   if (initialValue instanceof Function) return initialValue();
   return initialValue;
@@ -13,7 +12,6 @@ export default function useLocalStorage(key, initialValue) {
 
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
-    console.log(`set ${key} to ${value}`);
   }, [key, value]);
 
   return [value, setValue];
